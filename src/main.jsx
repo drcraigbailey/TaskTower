fe11e8bf78455ab6e2cb1fec1dd60342f71408d1
drcraigbailey <drcraig.bailey@gmail.com>
2026-07-05 15:40:24 +0100

    Mount household feature data provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AdultHouseholdProvider } from './context/AdultHouseholdContext.jsx'
 import { TaskTowerProvider } from './context/TaskTowerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
       <TaskTowerProvider>
-        <App />
+        <AdultHouseholdProvider>
+          <App />
+        </AdultHouseholdProvider>
       </TaskTowerProvider>
     </HashRouter>
   </StrictMode>,
